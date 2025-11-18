bab3e06b9e20bbb5e0c3537f823d2280593eeefd
sai kiran <saikiran@sais-MacBook-Air.local>
2025-11-18 05:35:25 +0530

    updated the url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json())
 
 app.use(cors({
-origin: "http://localhost:3000",  
+origin: "https://e-t-e-authencation-frontend.vercel.app/",  
   credentials: true,      
 }))
 
